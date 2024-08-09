eba145a4d81b2ea3030205c5796cf082c89c6ca7
Pavel <troshpaw@gmail.com>
2024-08-09 09:35:32 +0300

    lesson 11 in progress - website layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import Header from './Header';
-import Technologies from './Technoligies';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png'></img>
+      </header>
+      <nav className='nav'>
+        <div>Profile</div>
+        <div>Messages</div>
+      </nav>
+      <div className='content'>
+        Main content
+      </div>
     </div>
   );
 }
--- src/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-      <div>
-        <h4>The Way of the Samurai</h4>
-      </div>
-    );
-  }
-
-  export default Header;
