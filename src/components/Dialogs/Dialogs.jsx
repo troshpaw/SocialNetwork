4c709b62403e49865aea18d30e19188e6ad5496f
Pavel <troshpaw@gmail.com>
2024-08-15 10:32:51 +0300

    end lesson 21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,30 @@
-import style from './Dialogs.module.css';
+import styles from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsIitems}>
+                <div className={styles.dialog + ' ' + styles.active}>
+                    Dimych
+                </div>
+                <div className={styles.dialog}>
+                    Sveta
+                </div>
+                <div className={styles.dialog}>
+                    Dmitriy
+                </div>
+                <div className={styles.dialog}>
+                    Masha
+                </div>
+                <div className={styles.dialog}>
+                    Victor
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hello!</div>
+                <div className={styles.message}>How are you?</div>
+                <div className={styles.message}>Bye-bye!</div>
+            </div>
         </div>
     );
 }
