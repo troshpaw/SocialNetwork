dab5fdb46953ce9f4aa449db9d8fb8324016bd4e
Pavel Troshin <troshpaw@gmail.com>
2024-08-20 22:10:21 +0300

    the end 32 lesson - added function addPost in state

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
 import styles from './Dialogs.module.css';
@@ -7,14 +8,28 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} />);
     let messagesElements = props.state.messages.map(m => <Message message={m.message} />);
 
+    let newMessagelement = React.createRef();
+
+    let addMessage = () => {
+        let text = newMessagelement.current.value;
+        alert(text);
+    };
+
     return (
         <div className={styles.dialogs}>
             <div className={styles.dialogsItems}>
                 {dialogsElements}
             </div>
-
-            <div className={styles.messages}>
-                {messagesElements}
+            <div>
+                <div className={styles.messages}>
+                    {messagesElements}
+                </div>
+                <div>
+                    <textarea ref={newMessagelement}></textarea>
+                </div>
+                <div>
+                    <button onClick={addMessage}>Add message</button>
+                </div>
             </div>
         </div>
     );
--- src/redux/state.js
+++ src/redux/state.js
@@ -4,7 +4,7 @@ const state = {
             { id: 1, message: 'Hi, how are you?', likesCount: 10 },
             { id: 2, message: 'This is my first post!', likesCount: 15 }
         ]
-    },  
+    },
     dialogsPage: {
         dialogs: [
             { id: 1, name: 'Dimych' },
@@ -12,13 +12,24 @@ const state = {
             { id: 3, name: 'Alex' },
             { id: 4, name: 'Masha' },
             { id: 5, name: 'Victor' }
-          ],    
-          messages: [
+        ],
+        messages: [
             { id: 1, message: 'Hello!' },
             { id: 2, message: 'How are you?' },
             { id: 3, message: 'Bye-bye!' }
-          ]
+        ]
     }
+};
+
+export const addPost = (message) => {
+
+    const post = {
+        id: 3,
+        message: message,
+        likesCount: 0
+    };
+
+    state.profilePage.posts.push(post);
 };
 
 export default state;
