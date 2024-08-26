2f2f2e34fb62598187e2499ee94397376065c27a
system administrator <asubgb8@yandex.ru>
2024-08-26 10:14:22 +0300

    35 lesson the end - refactoring render import/export

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import state from './redux/state'
 import { addPost, updateNewPostText } from './redux/state';
 import { addMessage, updateNewMessageText } from './redux/state';
+import { subscrube } from './redux/state';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -22,6 +23,8 @@ let rerenderEntireTree = (state) => {
 };
 
 rerenderEntireTree(state);
+
+subscrube(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
