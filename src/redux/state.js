f8a28b0dc0026f163cbfb08da847bec3f579d8f9
Pavel <troshpaw@gmail.com>
2024-08-28 11:44:20 +0300

    40 - in progress

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,6 +1,6 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
-const ADD_MESSAGE = 'ADD-MESSAGE';
+const SEND_MESSAGE = 'SEND-MESSAGE';
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
 let store = {
@@ -44,7 +44,7 @@ let store = {
     },
 
     dispatch(action) {
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
 
             let newPost = {
                 id: 3,
@@ -56,12 +56,12 @@ let store = {
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
 
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
 
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
 
-        } else if (action.type === 'ADD-MESSAGE') {
+        } else if (action.type === SEND_MESSAGE) {
 
             let newMessage = {
                 id: 4,
@@ -72,7 +72,7 @@ let store = {
             this._state.dialogsPage.newMessageText = '';
             this._callSubscriber(this._state);
 
-        } else if (action.type === 'UPDATE-NEW-MESSAGE-TEXT') {
+        } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
 
             this._state.dialogsPage.newMessageText = action.newText;
             this._callSubscriber(this._state);
@@ -87,7 +87,7 @@ export const updateNewPostTextActionCreator = (text) => {
     return { type: UPDATE_NEW_POST_TEXT, newText: text }
 }
 
-export const addMessageActionCreator = () => ({ type: ADD_MESSAGE })
+export const addMessageActionCreator = () => ({ type: SEND_MESSAGE })
 
 export const onMessageChangeActionCreator = (text) => {
     return { type: UPDATE_NEW_MESSAGE_TEXT, newText: text }
