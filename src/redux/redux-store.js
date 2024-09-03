5ec6e81ef76dc5ba32ff56b6d4beca046afd94af
Pavel <troshpaw@gmail.com>
2024-09-03 11:48:22 +0300

    added Redux Toolkit (commented out)

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,5 @@
 import { combineReducers, legacy_createStore as createStore } from "redux";
+// import { configureStore } from "@reduxjs/toolkit";
 import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
@@ -10,5 +11,13 @@ let reducers = combineReducers({
 });
 
 let store = createStore(reducers);
+
+// const store = configureStore({
+//     reducer: {
+//         dialogsPage: dialogsReducer,
+//         profilePage: profileReducer,
+//         sidebar: sidebarReducer
+//     }
+// });
 
 export default store;
