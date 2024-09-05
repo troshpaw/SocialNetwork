40e365e963b13bdca3d363b5da81ce6e73bf405b
Pavel <troshpaw@gmail.com>
2024-09-05 08:49:27 +0300

    refactoring of the profile reducer is completed

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -11,30 +11,20 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_POST: {
-            let newPost = {
-                id: 3,
-                message: state.newPostText,
-                likesCount: 0
+        case ADD_POST:
+            let messageText = state.newPostText;
+            
+            return {
+                ...state,
+                newPostText: '',
+                posts: [...state.posts, {id: 3, message: messageText, likesCount: 0 }]
             };
 
-            let stateCopy = { ...state };
-            stateCopy.posts = [...state.posts];
-
-            stateCopy.posts.push(newPost);
-
-            stateCopy.newPostText = '';
-
-            return stateCopy;
-        }
-
-        case UPDATE_NEW_POST_TEXT: {
-            let stateCopy = { ...state };
-
-            stateCopy.newPostText = action.newText;
-
-            return stateCopy;
-        }
+        case UPDATE_NEW_POST_TEXT:
+            return {
+                ...state,
+                newPostText: action.newText
+            };
 
         default:
             return state;
