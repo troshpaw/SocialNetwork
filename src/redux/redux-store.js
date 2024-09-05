cc3fccf1418acc95ed30cc528e596de274982c3b
Pavel <troshpaw@gmail.com>
2024-09-05 09:03:14 +0300

    legacy redux disabled, redux toolkit enabled

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,23 +1,23 @@
-import { combineReducers, legacy_createStore as createStore } from "redux";
-// import { configureStore } from "@reduxjs/toolkit";
+// import { combineReducers, legacy_createStore as createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import dialogsReducer from "./dialogs-reducer";
 import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
 
-let reducers = combineReducers({
-    dialogsPage: dialogsReducer,
-    profilePage: profileReducer,
-    sidebar: sidebarReducer
-});
-
-let store = createStore(reducers);
-
-// const store = configureStore({
-//     reducer: {
-//         dialogsPage: dialogsReducer,
-//         profilePage: profileReducer,
-//         sidebar: sidebarReducer
-//     }
+// let reducers = combineReducers({
+//     dialogsPage: dialogsReducer,
+//     profilePage: profileReducer,
+//     sidebar: sidebarReducer
 // });
+
+// let store = createStore(reducers);
+
+const store = configureStore({
+    reducer: {
+        dialogsPage: dialogsReducer,
+        profilePage: profileReducer,
+        sidebar: sidebarReducer
+    }
+});
 
 export default store;
