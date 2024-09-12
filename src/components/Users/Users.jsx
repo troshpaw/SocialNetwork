230aa6de9d5ad32195c91dc9e8b3848e20181ed9
Pavel Troshin <troshpaw@gmail.com>
2024-09-13 00:11:08 +0300

    54 - used componentDidMount and added AJAX request

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,13 +5,14 @@ import userPhoto from './../../assets/images/user.png';
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
+    // }
 
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
-                // this.props.setUsers(response.data.items);
-                props.setUsers(response.data.items);
+                this.props.setUsers(response.data.items);
             })
             .catch(error => {
                 console.log(error);
