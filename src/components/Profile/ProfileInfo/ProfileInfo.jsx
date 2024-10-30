ec2b99f4a1948d9df7079801da5bbfaf504f2cf5
system administrator <asubgb8@yandex.ru>
2024-10-30 13:40:36 +0300

    71 - added ProfileStatus component with local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import styles from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
 import userPhoto from '../../../assets/images/user.png';
+import ProfileStatus from './ProfileStatus/ProfileStatus';
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -9,12 +10,13 @@ const ProfileInfo = (props) => {
 
     return (
         <div>
-            <div className={styles.imageContent}>
+            {/* <div className={styles.imageContent}>
                 <img src='https://res.cloudinary.com/whydonate/image/upload/v1/whydonate-production/user/fundraiser-background/d4cc3696-7a77-11e9-81ee-419f95339d87_rsez7s'></img>
-            </div>
+            </div> */}
             <div className={styles.descriptionBlock}>
                 <div><img src={props.profile.photos.large ? props.profile.photos.large : userPhoto} /></div>
                 <div>{props.profile.userId}</div>
+                <ProfileStatus status="Hello my friends!" />
             </div>
         </div>
     );
---
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+class ProfileStatus extends React.Component {
+    state = {
+        editMode: false
+    }
+
+    activateEditMode = () => {
+        // this.state.editMode = true
+        this.setState({ editMode: true });
+    }
+
+    deactivateEditMode() {
+        // this.state.editMode = false
+        this.setState({ editMode: false });
+    }
+
+    render() {
+        return (
+            <div>
+                {!this.state.editMode &&
+                    <div>
+                        <span onClick={this.activateEditMode}>{this.props.status}</span>
+                    </div>
+                }
+
+                {this.state.editMode &&
+                    <div>
+                        <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status} />
+                    </div>
+                }
+            </div>
+        )
+    }
+}
+
+export default ProfileStatus;
