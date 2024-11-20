aac52ca49e1e2afb4aa89821c2066a2dd2b24ba7
Pavel <troshpaw@gmail.com>
2024-11-20 15:29:05 +0300

    83 - added reselect for selectors UsersConatainer

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,6 +1,18 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const getUsers = (state) => {
     return state.usersPage.users;
 }
+
+export const getUsersSelector = (state) => {
+    return getUsers(state).filter(user => true);
+}
+
+export const getUsersSuperSelector = createSelector(getUsers,
+    (users) => {
+        return users.filter(user => true);
+    }
+)
 
 export const getPageSize = (state) => {
     return state.usersPage.pageSize;
